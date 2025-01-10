c4eb4f51dcce2f99523220decf555d5d6fed61b4
LRWF <Lechnolocy@foxmail.com>
2025-01-10 17:36:27 +0800

    Update config.sh

--- config.sh
+++ config.sh
@@ -1,5 +1,3 @@
 BUILD_QQNT_DATA_LIST=(
-  # 顺序为从新到旧，最新版在最上方
-  3.1.1-11223,https://dldir1.qq.com/qqfile/qq/QQNT/2355235c/linuxqq_3.1.1-11223
-  3.1.2-12912,https://dldir1.qq.com/qqfile/qq/QQNT/80d33f88/linuxqq_3.1.2-12912
+  3.2.15_241224,https://dldir1.qq.com/qqfile/qq/QQNT/Linux/QQ_3.2.15_241224_amd64_01.deb
 )
